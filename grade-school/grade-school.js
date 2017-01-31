5bf26da6017e2472fbbc8b09fb348cdf4253d0a5
mikal <mikal82@protonmail.com>
2017-01-31 12:19:18 +0100

    Grade school, next iteration

--- grade-school/grade-school.js
+++ grade-school/grade-school.js
@@ -1,36 +1,17 @@
 function School(){
-  this.students = [];
+  this.students = {};
+  
   this.add = function(student, grade){
-    this.students.push([student,grade]);
+    this.students[grade] = this.grade(grade).concat(student).sort()
   };
 
   this.grade = function(grade){
-    return this.students.reduce(function(acc,elem){
-      if (elem[1] == grade) {
-        acc.push(elem[0])
-      }
-      return acc
-    },[]).sort()
+    return this.students[grade] || []
   }
-
- this.list_grades = function(){
-   return  this.students.reduce(function(acc,elem){
-     if (!acc.includes(elem[1])) {
-       acc.push(elem[1])
-     }
-     return acc
-   },[])
- }
 
   this.roster = function(){
-    grades = this.list_grades();
-    output = {}
-    for (var iter = 0; iter < grades.length; iter++) {
-      output[grades[iter]] = this.grade(grades[iter]);
-    }
-    return output;
+    return this.students
   }
-
 }
 
 module.exports = School
