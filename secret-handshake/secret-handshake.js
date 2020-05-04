7b27b27530aabbbe27d53c9a8d793cfd33f9223b
mikal <mikal82@protonmail.com>
2020-05-04 11:20:34 +0200

    Secret handshake

--- secret-handshake/secret-handshake.js
+++ secret-handshake/secret-handshake.js
@@ -1,11 +1,12 @@
-//var reactions = ['wink', 'double blink', 'close your eyes', 'jump'];
 
-function secretHandshake(input) {
-// var bits = [0,1,2,3,4].map((x)=> {return ((2^x) & input) });
-// console.log(bits);
- return [];
-}
-
-//module.exports=secretHandshake;
-
+export function secretHandshake(input){
+  if (isNaN(input)) throw new Error("Handshake must be a number");
+  var action=[];
+  if (input & 1) action.push("wink");
+  if (input & 2) action.push("double blink");
+  if (input & 4) action.push("close your eyes");
+  if (input & 8) action.push("jump");
+  if (input & 16) action = action.reverse();
+  return action;
+};
 
